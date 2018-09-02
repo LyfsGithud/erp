763314b30baf5b28dc5af5d304c1970553b106aa
lyf123 <921798203@qq.com>
2018-09-02 12:00:46 +0800

    Test the sentence 'git diff xxx'.

--- Emmm.java
+++ Emmm.java
@@ -2,4 +2,11 @@ public class Emmm {
 
 	public static int i = 0;
 	public static Integer ii = new Integer(i);dddd
+	public static Integer ii = new Integer(i);dddd
+	public static Integer ii = new Integer(i);dddd
+	public static Integer ii = new Integer(i);dddd
+	public static Integer ii = new Integer(i);dddd
+	public static Integer ii = new Int
+	
+
 }
