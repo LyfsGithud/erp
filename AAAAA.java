bbd4b307165151d3f576198be353f6d3f3ce842c
lyf123 <921798203@qq.com>
2018-09-02 15:48:19 +0800

    Test the resettion VI

--- AAAAA.java
+++ AAAAA.java
@@ -9,6 +9,7 @@ public class AAAAA{
 		System.out.println("333333333333333");
 		System.out.println("444444444444444");
 		System.out.println("555555555555555");
+		System.out.println("666666666666666");
 	}
 
 }
