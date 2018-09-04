ed198418a809a8ebeb0dfc9f7d76db259236e13f
lyf123 <921798203@qq.com>
2018-09-04 12:06:17 +0800

    First try.

---
+++ AAAAA.java
@@ -0,0 +1,16 @@
+public class AAAAA{
+
+	public static void main(String[] args){
+		System.out.println("Hello Git211qqqqq!");	
+		System.out.println("ho Git211qqqqq!");	
+		System.out.println("ho Git211qqqqq!");
+		System.out.println("111111111111111");
+		System.out.println("222222222222222");
+		System.out.println("333333333333333");
+		System.out.println("444444444444444");
+		System.out.println("555555555555555");
+		System.out.println("666666666666666");
+		System.out.println("777777777777777");
+	}
+
+}
